3df5792905122b0a221c2ef37d4d7eceebf08bcc
Layssa Dias <layssa.fernanda@outlook.com.br>
2025-11-13 16:03:29 -0300

    project

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  e2e: {
+    env: {
+      apiUrl: process.env.API_URL || "https://serverest.dev/",
+    },
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.cy.ts",
+    supportFile: "cypress/support/e2e.ts",
+    video: true,
+    screenshotOnRunFailure: true
+  },
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome-report",
+    reportFileName: "full-regression-report",
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    reportPageTitle: "Desafio QA - Api",
+    embeddedScreenshots: true,
+    inlineAssets: true
+  }
+});
